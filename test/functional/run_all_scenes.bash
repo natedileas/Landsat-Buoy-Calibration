92fdd780a660b307c7d9aaf09a872331a937805c
Nathan Dileas <ndileas@gmail.com>
2016-06-20 15:25:00 -0400

    More testing.

--- test/functional/run_all_scenes.bash
+++ test/functional/run_all_scenes.bash
@@ -74,6 +74,19 @@ LC80170302014272LGN00) # modeled radiance is a nan (merra)
 # LC80150402013175LGN00
 #******* WARNING >>>>>>  UPBEAM--SGECO SAYS MATRIX NEAR SINGULAR
 
+#LC80410372014104LGN00
+# Traceback (most recent call last):
+#  File "./buoy-calib", line 41, in <module>
+#    cc.calc_all()
+#  File "/cis/ugrad/nid4986/Buoy_Calibration/bin/BuoyCalib.py", line 164, in calc_all
+#    self.calculate_buoy_information()
+#  File "/cis/ugrad/nid4986/Buoy_Calibration/bin/BuoyCalib.py", line 307, in calculate_buoy_information
+#    temp, pres, atemp, dewp = buoy_data.find_skin_temp(self, unzipped_file, depths[urls.index(url)])
+#  File "/cis/ugrad/nid4986/Buoy_Calibration/bin/buoy_data.py", line 292, in find_skin_temp
+#    T_zt = float(data[t][14])    # temperature data from closest hour
+# IndexError: index 19 is out of bounds for axis 0 with size 4
+
+
 
 L8_wo_wgrib=(LC80130332013145LGN00 
 LC80140332013104LGN01 
@@ -138,9 +151,9 @@ LT50410372011064PAC01
 LT50410372011048PAC01
 )
 
-for i in "${ids_8[@]}"
+for i in "${L8_wo_wgrib[@]}"
 do 
    echo $i
-   ./buoy-calib $i -rm
+   ./buoy-calib $i -r
    echo
 done

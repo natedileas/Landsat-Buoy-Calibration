b206ac08ebce6bc31ddbb856509ef7316abc17be
Nathan Dileas <ndileas@gmail.com>
2018-03-30 10:44:24 -0400

    Forgot data file for previewing

--- preview.py
+++ preview.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Preview a LANDSAT scene with the buoy drawn on it.')
     parser.add_argument('scene_id', help='LANDSAT scene ID. Examples: LC08_L1TP_017030_20170703_20170715_01_T1')
     parser.add_argument('-b', '--buoy_id', help='NOAA Buoy ID. Example: 45012', default=None)
+    parser.add_argument('-s', '--save', help='save an image out', default=False, action='store_true')
     # TODO NARR or MERRA
     # TODO add MODIS
     # TODO add band choice
@@ -16,4 +17,6 @@ if __name__ == '__main__':
     
     cv2.imshow('Landsat Preview', image)
     cv2.waitKey(0)
-    cv2.imwrite('landsat_preview.jpg', image)
+    
+    if args.save:
+        cv2.imwrite('landsat_preview.jpg', image)
